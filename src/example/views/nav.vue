74f1543e36d9ae60784ba8037c52da9e7bc78906
龚健 <gongjian@idea-group.cn>
2019-09-26 18:39:53 +0800

    111

--- src/example/views/nav.vue
+++ src/example/views/nav.vue
@@ -9,7 +9,7 @@
             text-color="#fff"
             active-text-color="#ffd04b"
         >
-            <el-submenu index="1">
+            <!-- <el-submenu index="1">
                 <template slot="title">
                     <i class="el-icon-location"></i>
                     <span>导航一</span>
@@ -26,19 +26,19 @@
                     <template slot="title">选项4</template>
                     <el-menu-item index="1-4-1">选项1</el-menu-item>
                 </el-submenu>
-            </el-submenu>
-            <el-menu-item index="2">
+            </el-submenu>-->
+            <el-menu-item index="1">
                 <i class="el-icon-menu"></i>
-                <span slot="title">导航二</span>
+                <span slot="title">button</span>
             </el-menu-item>
             <el-menu-item
-                index="3"
+                index="2"
                 disabled
             >
                 <i class="el-icon-document"></i>
-                <span slot="title">导航三</span>
+                <span slot="title">alink</span>
             </el-menu-item>
-            <el-menu-item index="4">
+            <el-menu-item index="3">
                 <i class="el-icon-setting"></i>
                 <span slot="title">导航四</span>
             </el-menu-item>
